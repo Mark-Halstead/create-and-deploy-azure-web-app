99cf3d5929f948771b82be8499b5eb820a63d9b6
Mark Halstead <103516032+Mark-Halstead@users.noreply.github.com>
2024-11-28 12:15:49 +0000

    Update create-and-deploy-web-app.ps1

--- create-and-deploy-web-app.ps1
+++ create-and-deploy-web-app.ps1
@@ -6,7 +6,7 @@ $webAppName = Read-Host "enter web app name"
 $gitRepoURL = Read-Host "enter git repository url:"  
 $branch = Read-Host "enter branch of repo you want to deploy:" 
 
-# Check if the resource group exists, if not create it
+# rg deployment
 $resourceGroup = Get-AzResourceGroup -Name $resourceGroupName -ErrorAction SilentlyContinue
 if (-not $resourceGroup) {
     Write-Host "Creating Resource Group: $resourceGroupName"
@@ -15,7 +15,7 @@ if (-not $resourceGroup) {
     Write-Host "Resource Group $resourceGroupName already exists."
 }
 
-# Check if the App Service Plan exists, if not create it
+# asp deployment
 $appServicePlanResource = Get-AzAppServicePlan -Name $appServicePlan -ResourceGroupName $resourceGroupName -ErrorAction SilentlyContinue
 if (-not $appServicePlanResource) {
     Write-Host "Creating App Service Plan: $appServicePlan"
@@ -24,7 +24,7 @@ if (-not $appServicePlanResource) {
     Write-Host "App Service Plan $appServicePlan already exists."
 }
 
-# Check if the Web App exists, if not create it
+# web app deployment
 $webApp = Get-AzWebApp -Name $webAppName -ResourceGroupName $resourceGroupName -ErrorAction SilentlyContinue
 if (-not $webApp) {
     Write-Host "Creating Web App: $webAppName"
@@ -33,7 +33,7 @@ if (-not $webApp) {
     Write-Host "Web App $webAppName already exists."
 }
 
-# Configure GitHub deployment for the Web App
+# getting the github repo url and deploying to web app
 Write-Host "Configuring GitHub deployment for $webAppName from public repository $gitRepoURL"
 $PropertiesObject = @{
     repoUrl = $gitRepoURL;
@@ -41,5 +41,7 @@ $PropertiesObject = @{
 }
 Set-AzResource -PropertyObject $PropertiesObject -ResourceGroupName $resourceGroupName -ResourceType "Microsoft.Web/sites/sourcecontrols" -ResourceName "$webAppName/web" -ApiVersion "2015-08-01" -Force
 
+
+# newly deployed app url
 Write-Host "Web App $webAppName configured to deploy from public GitHub repository $gitRepoURL."
 Write-Host "Browse to https://$webAppName.azurewebsites.net once the deployment is complete."
